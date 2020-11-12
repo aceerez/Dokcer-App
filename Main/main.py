8812d12912b9ffd51c88ba787594e9e088fa1284
aceerez <63843803+aceerez@users.noreply.github.com>
2020-11-13 00:21:13 +0200

    Update main.py
    Add Bar menu

--- Main/main.py
+++ Main/main.py
@@ -10,11 +10,19 @@ root = tk.Tk ()
 
 root.geometry ("1000x800")
 root.pack_propagate (0)
-root.resizable (0, 0)
+root.resizable(0, 0)
+
+bar_menu=Menu(root)
+root.config(menu=bar_menu)
 
 containerMenu = ContainerClass.Container(root)
 imageMenu = ImagesClass.Images(root)
 
-
+file_menu = Menu(bar_menu)
+bar_menu.add_cascade(label="Actions", menu=file_menu)
+file_menu.add_command(label="Load Container List",command=containerMenu.GetListDate)
+file_menu.add_command(label="Load Image List",command=imageMenu.GetListDate)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
 
 root.mainloop ()  # The mainloop for our tkinter Gui
